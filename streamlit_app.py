2b46f60023dc7ae02ead03990f14a9688a706679
rajboggala4learning <127152825+rajboggala4learning@users.noreply.github.com>
2023-03-15 02:07:30 -0400

    Update streamlit_app.py
    corrected code with the right method .fetchall() to get all the rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
 # code line to display all the lines
-my_data_rows = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text("The fruit load list contains:")
 # streamlit.text(my_data_row)
