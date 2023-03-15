fc50403f63c03e460e0d6808cfba188456494abc
rajboggala4learning <127152825+rajboggala4learning@users.noreply.github.com>
2023-03-15 00:00:57 -0400

    Update streamlit_app.py
    comment the raw json display

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writed the data to the screen - json as string to display
+#streamlit.text(fruityvice_response.json()) # just writed the data to the screen - json as string to display
 
 # use pandas package to normalize/flatten/parse the json data for fitting it into a dataframe/tabular form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
