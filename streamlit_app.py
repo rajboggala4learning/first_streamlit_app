6bcf088fb6726f3c9415ff11dcb2bd793fb983fd
rajboggala4learning <127152825+rajboggala4learning@users.noreply.github.com>
2023-03-15 00:09:02 -0400

    Update streamlit_app.py
    added code to  use the 'fruit_choice' variable to pass instead of hardcoding the value as 'Kiwi'

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,9 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+# use the 'fruit_choice' variable to pass instead of hardcoding the value as 'Kiwi'
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json()) # just writed the data to the screen - json as string to display
 
