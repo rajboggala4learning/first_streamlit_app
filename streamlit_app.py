022011c473c8b390e06cc1d07cad8e07563e98ad
rajboggala4learning <127152825+rajboggala4learning@users.noreply.github.com>
2023-03-15 02:22:34 -0400

    Update streamlit_app.py
    corrected syntax/typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,4 +76,4 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 # Control of flow test 
 # This will not work correctly, but just go with it for now
-my_cur.execute"'insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
