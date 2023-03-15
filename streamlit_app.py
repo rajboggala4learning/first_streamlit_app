934d5cd66964861946cdeaaa15686190ba8e9682
rajboggala4learning <127152825+rajboggala4learning@users.noreply.github.com>
2023-03-15 01:59:01 -0400

    Update streamlit_app.py
    Added code lines to query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #  Display the fruityvice table on the page
 streamlit.dataframe(fruityvice_normalized)
 
-# added code line to import snowflake python connector package or library
+# Added code line to import snowflake python connector package or library
 import snowflake.connector
+
+# Added code lines to query Our Trial Account Metadata 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
